0b5270cfca05eb1f57c17471b9ca73b216f99cd2
yf42 <yuanfang42@gmail.com>
2024-07-16 12:51:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'f9303535-0937-4305-b670-53a2f97c69e8';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
